3e1598b3d3cbc4d25afff5c464ef799b11b28724
Forrest <forrest@ccbn.us>
2018-11-28 22:07:19 -0600

    pypi

--- setup.py
+++ setup.py
@@ -11,11 +11,11 @@ setup(
     long_description_content_type = "text/markdown",
     url = "https://github.com/Waffles32/memosa",
     packages = find_packages(),
-    classifiers = (
+    classifiers = [
         "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ),
+    ],
 	include_package_data = True,
 	install_requires = open('requirements.txt').read().splitlines(),
 	python_requires='>3.6',
